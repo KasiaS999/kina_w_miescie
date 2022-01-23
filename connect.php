133421e75c0ff048598cbf0463d0e7fb3acf64c4
Katarzyna Such <57150290+KasiaS999@users.noreply.github.com>
2022-01-23 09:19:39 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 
 <?php
-$host = "ec2-52-213-119-221.eu-west-1.compute.amazonaws.com";
-$user = "ecmvihlisjaoxv";
-$pass = "436f850b574664ad17064c5ff378ea790b099749d1d81b6221731f2d422bb33b";
-$db = "d724guo0o18743";
-$port = "5432";
+$host = "";
+$user = "";
+$pass = "";
+$db = "";
+$port = "";
 
 $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
  or die ("Could not connect to server\n");
